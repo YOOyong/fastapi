599473417533e6d059b6c0c4dd1fa1f76c3c989e
YOOyong <phg5656@gmail.com>
2024-01-04 00:27:06 +0900

    sqlite db init create table

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class Question(Base):
+    __tablename__ = "question"
+
+    id = Column(Integer, primary_key=True,)
+    subject = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
+
+
+class Answer(Base):
+    __tablename__ = "answer"
+
+    id = Column(Integer, primary_key=True)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
+    question_id = Column(Integer, ForeignKey("question.id"))
+    # answer 에서 question 모델을 참조하기 위해 넣은 변수.
+    # answer.question.subject 식으로 접근 가능하게 해준다.
+    question = relationship("Question", backref="answers") # backref 로 역참조가 가능하게 설정.
+
+
+
+
+
+
