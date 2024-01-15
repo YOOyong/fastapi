4abb60324849318995fea5ecb182055c5169e07f
YOOyong <phg5656@gmail.com>
2024-01-15 22:23:30 +0900

    answer create done

--- app/domain/answer/answer_schema.py
+++ app/domain/answer/answer_schema.py
@@ -9,7 +9,7 @@ class AnswerCreate(BaseModel):
     def not_empty(cls, v):
         if not v or not v.strip():
             raise ValueError('빈 값은 허용되지 않습니다.')
-
+        return v
 
 class Answer(BaseModel):
     id: int
--- app/domain/question/question_schema.py
+++ app/domain/question/question_schema.py
@@ -1,6 +1,5 @@
 import datetime
 from pydantic import BaseModel, field_validator
-from typing import List
 from domain.answer.answer_schema import Answer
 
 class Question(BaseModel):
@@ -8,7 +7,7 @@ class Question(BaseModel):
     subject: str
     content: str
     create_date: datetime.datetime
-    answers: List[Answer] = [] #모델에서 backref 를 설정했기에 가져오기 가능
+    answers: list[Answer] = [] #모델에서 backref 를 설정했기에 가져오기 가능
 
 
 class QuestionCreate(BaseModel):
